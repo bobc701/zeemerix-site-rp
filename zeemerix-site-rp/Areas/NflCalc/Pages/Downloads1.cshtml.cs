94054f82ab079cebd9784b479abc12d0f3508f8d
Bob C <bobc@4bccx.com>
2021-03-30 09:34:56 -0400

    Add project files.

---
+++ zeemerix-site-rp/Areas/NflCalc/Pages/Downloads1.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication23.Areas.NflCalc.Pages
+{
+    public class Downloads1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ zeemerix-site-rp/Areas/iOS/Pages/Contact1.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication23.Areas.iOS.Pages
+{
+    public class Contact1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
